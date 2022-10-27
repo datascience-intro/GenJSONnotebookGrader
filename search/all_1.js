cc8bbd8736b3b6ca6ac9f46c1f13fa3277aae0d8
Oskar Åsbrink <asbrinkoskar@gmail.com>
2022-10-27 21:09:46 +0200

    Updated docs

--- search/all_1.js
+++ search/all_1.js
@@ -2,6 +2,5 @@ var searchData=
 [
   ['assignment_0',['Assignment',['../class_assignment_notebook_1_1_assignment.html',1,'AssignmentNotebook']]],
   ['assignmentnotebook_1',['AssignmentNotebook',['../class_assignment_notebook_1_1_assignment_notebook.html',1,'AssignmentNotebook.AssignmentNotebook'],['../md__notebook_grader__assignment_notebook__r_e_a_d_m_e.html',1,'(Global Namespace)']]],
-  ['autograder_2',['Autograder',['../class_auto_grader_1_1_autograder.html',1,'AutoGrader']]],
-  ['autograder_20for_20notebooks_20in_20json_20format_3',['Autograder for notebooks in JSON format',['../index.html',1,'']]]
+  ['autograder_2',['Autograder',['../class_auto_grader_1_1_autograder.html',1,'AutoGrader']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['assignmentnotebook_0',['AssignmentNotebook',['../md__notebook_grader__assignment_notebook__r_e_a_d_m_e.html',1,'']]],
-  ['autograder_20for_20notebooks_20in_20json_20format_1',['Autograder for notebooks in JSON format',['../index.html',1,'']]]
+  ['assignmentnotebook_0',['AssignmentNotebook',['../md__notebook_grader__assignment_notebook__r_e_a_d_m_e.html',1,'']]]
 ];
