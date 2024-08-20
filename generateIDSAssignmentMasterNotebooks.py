9785d87584c0f6355f73a53e2091b9c24a74d987
Benny Avelin <>
2024-08-20 09:40:54 +0200

    First commit

---
+++ generateIDSAssignmentMasterNotebooks.py
@@ -0,0 +1,8 @@
+from NotebookGrader import IDSCourse
+
+idsCourse = IDSCourse()
+
+idsCourse.makeAssignmentNotebooks(notebook_type='problem')
+idsCourse.makeAssignmentNotebooks(notebook_type='problem_TEST')
+idsCourse.makeAssignmentNotebooks(notebook_type='solution_TEST')
+idsCourse.makeAssignmentNotebooks(notebook_type='problem_solution')
---
+++ generateIDSNotebooks.py
@@ -0,0 +1,4 @@
+from NotebookGrader import IDSCourse
+
+course = IDSCourse()
+course.to_nb()
