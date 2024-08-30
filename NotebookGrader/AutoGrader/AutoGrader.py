41baa3d41705bc75514a1c0b79836f84abcb963e
Benny Avelin <>
2024-08-30 16:13:03 +0200

    Trying to fix upload file html bug

--- NotebookGrader/AutoGrader/AutoGrader.py
+++ NotebookGrader/AutoGrader/AutoGrader.py
@@ -244,10 +244,11 @@ class Autograder:
                                     self.prepareNotebookForGrading(studentSubmissionFileName,masterNotebookFileName,student_id) # <<< .ipynb, .scala, .r, etc (not dbc)
                                 else:
                                     gradeDict = self.safeGradeNotebook(studentSubmissionFileName,masterNotebookFileName,student_id, assName = self.assignment.attributes['name']) # <<< .ipynb, .scala, .r, etc (not dbc)
-
+                                    print("gradeDict created")
                                     grade = gradeDict['lx_problem_total_scored_points']
                                     comment = gradeDict['text_response'].replace('#','')
                                     #writeResponseFile
+                                    print("Trying to write response file")
                                     if (gradeDict['Response_Notebook'] != ''): #<< json dict << check if it is dbc (zip + remove) or ipynb
                                         self.writeResponseFile(gradeDict['Response_Notebook'],student_id,submission['attempt'],studentSubmissionFileName)
 
@@ -453,6 +454,7 @@ class Autograder:
             finalGradeDict, stdOutString = graded_ass_nb.extractResult() # Done
             finalGradeDict.update({'text_response': stdOutString}) # Done
             #we can add this in assignmentnotebook class probably
+            print("Trying to add summary")
             finalGradeDict,final_graded_nb = self.addSummary(finalGradeDict,graded_ass_nb,graded_nb) # Done)
             # cannot do the following in AssignmentNotebook/extractResult so do it here instead
 
